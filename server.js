9b3311d9b996cade9e234f38e3fac0d9b7883baa
charlesthms <charles.thms77@gmail.com>
2020-05-09 18:47:58 +0200

    v1.0

---
+++ server.js
@@ -0,0 +1,27 @@
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+
+
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function(socket){
+
+	console.log('User connected...')
+
+	socket.on('disconnect', function(){
+		console.log('User disconnected...')
+	})
+
+	socket.on('chat message', function(msg){
+		console.log('Message reçu (server) : '+msg)
+		io.emit('chat message', msg)
+	})
+})
+
+http.listen(3000, function() {
+	console.log('Server running on port 3OOO')
+})
